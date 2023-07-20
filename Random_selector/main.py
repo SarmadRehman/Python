d5dab6e5b638a3e28d2f7b6cb85ec9af2399ee98
Sarmad Rehman <sarmadr02@gmail.com>
2023-07-20 19:06:05 +0500

    file-handling mod:2 Programming in Python

--- Random_selector/main.py
+++ Random_selector/main.py
@@ -3,6 +3,8 @@ import random
 f_name = input("Type the file name: ")
 b = "Random_selector/" + f_name + ".txt"
 f = open(b)  # "r" omitted as it's the default
+
 f_content = f.read()
+print(type(f_content))
 f_content_list = f_content.split("\n")
 print(random.choice(f_content_list))
