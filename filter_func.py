837c370e765138fd884e77128ae214ae1175677b
Sarmad Rehman <sarmadr02@gmail.com>
2023-09-11 06:41:51 -0700

    new practices filter_f and  list_comp

--- filter_func.py
+++ filter_func.py
@@ -1,6 +1,7 @@
 ages = [5, 12, 17, 18, 24, 32]
 
-
+# filter() function expects the provided function to return a boolean 
+# value (True or False).
 def myFunc(x):
     if x < 18:
         return False
